c3987d6e01485edecbc354ef558f29bc042b8c84
Nguyen Cong Nhat <51161694+ckan719@users.noreply.github.com>
2020-12-16 21:21:22 +0700

    ''

--- backend/test.py
+++ backend/test.py
@@ -4,7 +4,7 @@ data = {}
 data['category_name'] = 'category_name'
 data['description'] = 'description'
 data['picture'] = ''
-report = requests.post('https://192.168.1.13:8080/insert_categories', json=data)
+report = requests.post('http://192.168.1.13:8080/insert_categories', json=data)
 print(report.text)
 
 # data = {}
